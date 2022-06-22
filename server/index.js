c03e9a43a168d986dc002886d94815b15432e547
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-22 14:07:23 +0200

    express node server issues

--- server/index.js
+++ server/index.js
@@ -2,25 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+
+//express middelware to connect to application,set the starting path for routes
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-//express middelware to connect to application,set the starting path for routes
-app.use("/posts", postRoutes);
-
 const CONNECTION_URL =
   "mongodb+srv://Thato:Thato123@cluster0.ku883.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 6000;
 
-const PORT = process.env.PORT || 5000;
-
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log(`Server is running on port:http://localhost:${PORT}`))
-  .catch((error) => console.log(error.message));
-
-mongoose.set("useFindAndModify", false);
+await mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server is running on port:http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
