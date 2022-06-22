bd2eaee2fb5e4e247727e85e7acf00f7923b938c
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-22 12:52:36 +0200

    package chnage

--- server/index.js
+++ server/index.js
@@ -2,14 +2,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import postRoutes from "./routes/posts.js";
 
-const express = express();
+const app = express();
+
+//express middelware to connect to application,set the starting path for routes
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
 const CONNECTION_URL =
   "mongodb+srv://Thato:Thato123@cluster0.ku883.mongodb.net/?retryWrites=true&w=majority";
 
-const PORT = process.env.PORT || 6000;
-mongoose
-  .connect(CONNECTION_URL)
-  .then(() => console.log("Server is running on port:${PORT}"))
+const PORT = process.env.PORT || 5000;
+
+await mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(`Server is running on port:${PORT}`))
   .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
