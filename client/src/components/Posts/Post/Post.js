41453d534b2e352c4843ec6db21206685b9f8c81
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-25 12:42:39 +0200

    savg background for the application+post+cardContainer

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -35,7 +35,28 @@ const Post = ({ post }) => {
           <MoreHorizIcon fontSize="default" />
         </Button>
       </div>
-      <div></div>
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary">
+          {post.tags.map((tag) => `#${tag} `)}
+        </Typography>
+      </div>
+      <CardContent>
+        {" "}
+        <Typography className={classes.title} variant="h5" gutterBottom>
+          {post.message}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" onClick={{}}>
+          <ThumbUpAltIcon size="small" />
+          Like
+          {post.likeCount}
+        </Button>
+        <Button size="small" color="primary" onClick={{}}>
+          <DeleteIcon size="small" />
+          Delete
+        </Button>
+      </CardActions>
     </Card>
   );
 };
