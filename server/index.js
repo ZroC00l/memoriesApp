5e704270eed546b497af65cad0011a0b02f02c02
ZroC00l <giftgabanakgosi@gmail.com>
2022-06-30 10:09:17 +0200

    added dotenv file plus dotenv package+always remove spaces in dotenv variable intializations, and remember no strings around the initialized variables

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "40mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "40mb", extended: true }));
@@ -14,12 +16,11 @@ app.use(cors());
 //express middelware to connect to application,set the starting path for routes
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://Thato:Thato123@cluster0.ku883.mongodb.net/?retryWrites=true&w=majority";
+//const CONNECTION_URL ="mongodb+srv://Thato:Thato123@cluster0.ku883.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 await mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server is running on port:http://localhost:${PORT}`)
