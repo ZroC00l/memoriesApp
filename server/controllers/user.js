c9d885268b2e366c9f0e6cabbf9e76636aca2695
ZroC00l <giftgabanakgosi@gmail.com>
2022-07-05 13:25:57 +0200

    backend authentication methods signup+signin

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,6 +2,45 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/user.js";
 
-export const signin = async (req, res) => {};
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
 
-export const signup = async (req, res) => {};
+  try {
+    const existingUer = await User.findOne({ email });
+    if (!existingUer) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUer.password
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signup = async (req, res) => {
+  const { firstName, lastName, email, password, confirmPassword } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Passwords do not match" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+    res.status(201).json({ token });
+  } catch (error) {
+    console.log(error);
+  }
+};
