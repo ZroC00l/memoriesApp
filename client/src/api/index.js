29e2852eb896f6398a77c3becec003e7f39c85d8
ZroC00l <giftgabanakgosi@gmail.com>
2022-07-06 08:24:37 +0200

    frontend api changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,15 +1,19 @@
 import axios from "axios";
 
-const url = "https://memory-silo.herokuapp.com/posts";
+const API = axios.create({
+  baseURL: "http://localhost:5000/",
+});
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
+//const url = "https://memory-silo.herokuapp.com/posts";
+
+export const fetchPosts = () => API.get("/posts");
+export const createPost = (newPost) => API.post("/posts", newPost);
 export const updatePost = (id, updatedPost) =>
-  axios.patch(`${url}/${id}`, updatedPost);
+  API.patch(`/posts/${id}`, updatedPost);
 
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
 
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 
 export const signin = () =>
   axios.post("http://localhost:3000/auth", {
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -69,15 +69,34 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
   const { id } = req.params;
 
+  //check if user is authenticated before they can perform as specific action
+  if (!req.userId) {
+    return res
+      .status(401)
+      .json({ message: "You must be logged in to perform this action" });
+  }
+
   if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send(`No post with id: ${id}`);
 
   const post = PostMessage.findById(id);
-  const updatedPost = await PostMessage.findByIdAndUpdate(
-    id,
-    { $inc: { likeCount: 1 } },
-    { new: true }
-  );
+
+  /*check if user id is in the like section or not if the user already 
+  liked the post clicking on it negates the like action*/
+  const index = post.likes.findIndex((id) => id === String(req.userId));
+
+  //means the user has not liked the post yet so we add the user id to the likes section
+  if (index === -1) {
+    post.likes.push(req.userId);
+  } else {
+    //means the user has liked the post so we remove the user id from the likes section
+    //post.likes.splice(index, 1);
+    post.likes = post.likes.filter((id) => id !== String(req.userId));
+  }
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+    new: true,
+  });
 
   res.json(updatedPost);
 };
